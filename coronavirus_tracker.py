290686a711c9789be35dea9c07be060dafccd6ee
LonelyWolf54i <jasotomayort9908@gmail.com>
2020-03-29 14:31:26 -0500

    Added script that retrieves the data

---
+++ coronavirus_tracker.py
@@ -0,0 +1,60 @@
+
+import re
+from bs4 import BeautifulSoup
+from MyPythonLibs.prettyit import prettylist
+import urllib
+from datetime import datetime
+import requests
+import warnings
+import traceback
+import sys
+
+
+def warn(*args, **kwargs):
+    pass
+
+
+warnings.warn = warn
+
+
+class AppURLopener(urllib.request.FancyURLopener):
+    version = "Mozilla/5.0"
+
+
+website_url = r"https://www.worldometers.info/coronavirus/"
+header = ['Country', 'TotalCases', 'NewCases', 'TotalDeaths',
+          'NewDeaths', 'TotalRecovered', 'ActiveCases', 'Critical',
+          'TotCases']
+
+
+def track(country_name):
+    try:
+        opener = AppURLopener()
+        response = opener.open(website_url)
+        website_content = response.read()
+        date = datetime.now().strftime("%H:%M %x")
+        if len(sys.argv) == 2: country_name = sys.argv[1]
+        soup = BeautifulSoup(website_content, 'lxml')
+        rows = [str(r) for r in soup.find_all(id="main_table_countries_today")]
+        soup2 = BeautifulSoup(''.join(rows), 'lxml')
+        countries_data = re.search(country_name+'(\n(\+?[0-9]+,?[0-9]* *)?){8}',
+                         soup2.get_text()).group().split("\n")
+        countries_data[3] = countries_data[3].strip()
+        data = {}
+        for i in range(len(header)):
+            data[header[i]] = countries_data[i]
+        country_data = prettylist(data)
+        print(country_data)
+        print(f'\nDate: {date}')
+        sys.stdout.flush()
+        return country_data, date
+    except Exception as e:
+        with open("coronavirus-tracker-errors.txt", "a") as error_log:
+            error_log.write(f"\n\nDATE: {datetime.now().strftime('%H:%M %x')}\n")
+            error_log.writelines(traceback.format_exc())
+    except KeyboardInterrupt as ki:
+        print(ki)
+
+
+if __name__ == '__main__':
+    track("Ecuador")
